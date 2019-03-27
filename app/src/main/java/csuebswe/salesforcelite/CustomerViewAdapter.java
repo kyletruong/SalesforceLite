74fd44356ecb33edc2b3fe491c74260eed5348e4
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2019-03-27 11:19:24 -0700

    Added customerViewAdapter

---
+++ app/src/main/java/csuebswe/salesforcelite/CustomerViewAdapter.java
@@ -0,0 +1,71 @@
+package csuebswe.salesforcelite;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.TextView;
+import csuebswe.salesforcelite.controller.MainMenu;
+import csuebswe.salesforcelite.controller.MenuItem;
+
+public class CustomerViewAdapter extends RecyclerView.Adapter<CustomerViewAdapter.ViewHolder>{
+
+    private MainMenu mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    CustomerViewAdapter(Context context, MainMenu data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String option = mData.getMenuItem(position).toString();
+        holder.myTextView.setText(option);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.tvSalesforceLite);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    MenuItem getItem(int id) {
+        return mData.getMenuItem(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+
+}
