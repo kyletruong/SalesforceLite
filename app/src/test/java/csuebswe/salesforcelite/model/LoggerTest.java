5a8b93197783af5e389eb1d4dfae2e8a38f22830
kyletruong <kyletruong@gmail.com>
2019-03-29 14:52:39 -0700

    update unit test for logger model

--- app/src/test/java/csuebswe/salesforcelite/model/LoggerTest.java
+++ app/src/test/java/csuebswe/salesforcelite/model/LoggerTest.java
@@ -1,8 +1,11 @@
+/**
+ * @author Kyle Truong
+ */
+
 package csuebswe.salesforcelite.model;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class LoggerTest {
